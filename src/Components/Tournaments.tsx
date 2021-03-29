97d39031aa5161877805d35a218ff92d03439721
DijamGoran <diamgoran@gmail.com>
2021-03-30 01:26:16 +0300

    Fixed a typo

--- src/Components/Tournaments.tsx
+++ src/Components/Tournaments.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Tournaments = () => (
     <div className='tournaments-page' id='tournaments-page'>
-        <h1>merch</h1>
+        <h1>Tournaments</h1>
         <p>
             Our hosted tournaments in the future will be featured here.
         </p>
